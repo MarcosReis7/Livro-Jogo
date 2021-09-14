09d9f5d18301b046e3a2e57d8e1a43985f47e1f5
FelipeRodrigues05 <felipebr998@gmail.com>
2021-09-14 19:00:24 -0300

    Jogo

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+
+public class Game {	
+	
+//	1. vc sai da mansão e o jogo acaba pq vc desistiu
+//	2. vc ganha pq descobre quem é o assassino
+//	3. vc perde pq o assassino te mata
+	
+	public static void main(String[] args) {
+		Inicio();
+	}
+	
+	public static void Inicio() {
+		System.out.println("É aqui que começamos a nossa jornada");
+	}
+
+}
